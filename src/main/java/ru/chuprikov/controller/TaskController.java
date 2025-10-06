ae0d50325cf7ea81db62eb5722c544b78427f1c2
Den4ikCh <chuprikov.denis.vrn@gmail.com>
2025-10-06 17:33:18 +0300

    null

--- src/main/java/ru/chuprikov/controller/TaskController.java
+++ src/main/java/ru/chuprikov/controller/TaskController.java
@@ -1,19 +1,11 @@
 package ru.chuprikov.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.HttpHeaders;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
 import ru.chuprikov.Task;
 import ru.chuprikov.dao.TaskDAO;
-
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStream;
-import java.io.InputStreamReader;
 
 @Controller
 @RequestMapping("/task")
